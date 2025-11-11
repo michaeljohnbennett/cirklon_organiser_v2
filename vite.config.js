1e1ab268433f7e6bc2e362d9b21728cdc5caec84
??? <not@chance.com>
2025-11-11 14:55:17 +1100

    feat: Update deployment workflow to dynamically set base path based on branch

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
-  base: './',
+  base: process.env.BASE_PATH || './',
   build: {
     outDir: 'dist'
   }
